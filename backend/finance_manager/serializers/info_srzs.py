7a44df658a0418b77c3db0ac06f568a6a6619783
Jisun Park <jisun.park113@gmail.com>
2020-12-06 18:13:35 +0900

    StockInfo Serializer에서 없는 필드 제거

--- backend/finance_manager/serializers/info_srzs.py
+++ backend/finance_manager/serializers/info_srzs.py
@@ -5,10 +5,10 @@ from finance_manager.models.info import USStockInfo, KOStockInfo
 class USStockInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = USStockInfo
-        fields = ["ticker", "price", "reg_date"]
+        fields = ["ticker", "reg_date"]
 
 
 class KOStockInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = KOStockInfo
-        fields = ["ticker", "price", "reg_date"]
+        fields = ["ticker", "reg_date"]
